d5df0519e645e029f58baec52760fe6e1aff08fc
Gustavo Muniz <cgmds73@gmail.com>
2020-07-21 22:21:29 -0300

    Added manual round robin

---
+++ src/main/scala/wordcount/manual/Worker.scala
@@ -0,0 +1,27 @@
+package wordcount.manual
+
+import akka.actor.{Actor, ActorLogging}
+
+object Worker {
+  case class CountWords(text: String, task: Int)
+}
+
+class Worker extends Actor with ActorLogging{
+  import Worker._
+
+  val prefix = "[Worker]"
+  val results: Map[Int, Int] = Map()
+
+  override def receive: Receive = process(results)
+
+  def process(r: Map[Int,Int]): Receive = {
+    case CountWords(text, task) =>
+      val words = text.split(" ").length
+      log.info(s"$prefix task $task found $words in text: $text")
+      val newMapResults: Map[Int,Int] = r + (task -> words)
+      context.become(process(newMapResults))
+
+    case other =>
+      log.info(s"$prefix Unknown message: ${other.toString}")
+  }
+}
